4de7f027a8cc7a9edf0e1d67da0f99d469e519e7
Guillermo <g.parra.lopez@protonmail.com>
2025-11-06 20:42:11 +0100

    Test 2 GIT

--- FX/src/main/java/umu/tds/App.java
+++ FX/src/main/java/umu/tds/App.java
@@ -1,16 +1,12 @@
 package umu.tds;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class App extends Application {
