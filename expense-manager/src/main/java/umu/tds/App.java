2823eaae3a706bc44b625805c08dd29e61a428ae
Guillermo <g.parra.lopez@protonmail.com>
2026-01-03 03:37:30 +0100

    Sesion v1

--- expense-manager/src/main/java/umu/tds/App.java
+++ expense-manager/src/main/java/umu/tds/App.java
@@ -3,11 +3,15 @@ package umu.tds;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import umu.tds.vista.SceneManager;
+import umu.tds.modelo.Usuario;
+import umu.tds.controlador.ControladorSesion; 
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+    	Usuario userTest = new Usuario("PedroF");
+    	ControladorSesion.getInstancia().setUsuarioActual(userTest);
         //1. Vinculamos el Stage principal con nuestro SceneManager (Singleton)
         SceneManager.getInstancia().setStage(primaryStage);
 
