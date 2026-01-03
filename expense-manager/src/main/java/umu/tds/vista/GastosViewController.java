113f7a8f973fe201d24be551b3836f5ae21df2a4
Guillermo <g.parra.lopez@protonmail.com>
2026-01-03 02:34:44 +0100

    Added GastosView.fxml and GastosViewController

---
+++ expense-manager/src/main/java/umu/tds/vista/GastosViewController.java
@@ -0,0 +1,16 @@
+package umu.tds.vista;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import umu.tds.modelo.Gasto;
+
+public class GastosViewController {
+    @FXML
+    private TableView<Gasto> tablaGastos;
+
+    @FXML
+    public void initialize() {
+        
+    }
+    
+}
